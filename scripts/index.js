8d1248321a5d4993fc29481d7f1aebddb39e2038
Vladislav <70971642+YBHPG@users.noreply.github.com>
2025-04-18 21:50:16 +0300

    Создан объект карточки и функции добавления и удаления карточек со страницы.

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,39 @@
-// @todo: Темплейт карточки
+// Получаем шаблон карточки и контейнер для карточек
+const template = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+// Функция удаления карточки
+function deleteCard(event) {
+    const card = event.target.closest('.card');
+    card.remove();
+}
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard(cardData, deleteCallback) {
+    // Клонируем содержимое шаблона
+    const cardElement = template.cloneNode(true);
 
-// @todo: Функция удаления карточки
+    // Устанавливаем значения для карточки
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    cardTitle.textContent = cardData.name;
 
-// @todo: Вывести карточки на страницу
+    // Обработчик для кнопки удаления
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', deleteCallback);
+
+    return cardElement;
+}
+
+// Функция для вывода карточек на страницу
+function renderCards(cards) {
+    cards.forEach((cardData) => {
+        const cardElement = createCard(cardData, deleteCard);
+        placesList.append(cardElement);
+    });
+}
+
+// Выводим карточки из массива initialCards
+renderCards(initialCards);
